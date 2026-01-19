cca994bf4159c9b841c7ed7dfcd8e989126c3daf
Joël Tuinman <jetuinman@gmail.com>
2026-01-19 15:12:56 +0100

    Update script0_regenie_phenos_logCAC_new.R

--- OV19_0495/regenie/script0_regenie_phenos_logCAC_new.R
+++ OV19_0495/regenie/script0_regenie_phenos_logCAC_new.R
@@ -16,7 +16,7 @@ DALL <- merge(DALL, Dlink[,-1], by.x = "project_pseudo_id", by.y = "project_pseu
 
 #### data prep ####
 #load in UGLI linkage file
-Ddata <- read.table("/groups/umcg-lifelines/tmp02/projects/ov19_0495/1_LLphenotypes/Linkage_files/Cyto+UGLI+UGLI2+UGLI3_released_samples_linkage_all_plate_pos_withexclusionsUGLI2_updated.txt", header = T, stringsAsFactors = F)
+Ddata <- read.table("Linkage_file_path", header = T, stringsAsFactors = F)
 Ddata <- merge(Ddata, DALL, all.x = F, all.y = F, by.x = "PROJECT_PSEUDO_ID", by.y = "project_pseudo_id_LL")
 
 #remove duplicates
@@ -34,4 +34,5 @@ colnames(Ddata)[1] <- "IID"
 Ddata <- Ddata[,c("FID", "IID", "age", "gender", "logCAC", paste0("PC", 1:10))] #Remember to add age2 if needed
 
 write.table(Ddata, "dataF_data_logCAC.txt", sep = "\t", quote = F, row.names = F)
+
 
